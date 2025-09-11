14685d02a40b7bb7e8d168abead906c39f9cdb33
Abhinav Goel <abhinavgo1000@gmail.com>
2025-09-11 16:48:08 +0530

    Initial Commit

---
+++ Job Searcher/Job_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_SearcherApp.swift
+//  Job Searcher
+//
+//  Created by Abhinav Goel on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Job_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Job SearcherTests/Job_SearcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Job_SearcherTests.swift
+//  Job SearcherTests
+//
+//  Created by Abhinav Goel on 9/11/25.
+//
+
+import Testing
+@testable import Job_Searcher
+
+struct Job_SearcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
